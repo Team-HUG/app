0189a8673e9825d77ef45c2d9166aaaa69c12d5d
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-10-11 15:01:24 +0900

    tailwind 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#000000',
+        hightlight: '#DE781F',
+        hover: '#F5F5F5',
+        gray: '#AAAAAA',
+        footer: '#EEEEEE',
+        white: '#FFFFFF',
+      },
+      fontWeight: {
+        bold: 700,
+        medium: 500,
+        regular: 400,
+      },
+    },
   },
   plugins: [],
 };
