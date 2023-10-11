bddd00de42774c75830b33b97ff9c516938187ea
Ubinquitous <ubinquitous1@gmail.com>
2023-10-11 19:41:46 +0900

    어드민 게임리스트

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import OrderPage from './pages/OrderPage';
 import Prepare from './pages/Prepare';
+import AdminOrderList from './pages/AdminOrderList';
+import AdminGameList from './pages/AdminGameList';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/order" element={<OrderPage />} />
+        <Route path="/admin/order-list" element={<AdminOrderList />} />
+        <Route path="/admin/game-list" element={<AdminGameList />} />
         <Route path="/" element={<div />} />
         <Route path="/prepare" element={<Prepare />} />
       </Routes>
