a16d54ab16d8ed8c30fdf0b5ad439d204e90617e
unrin <momlin069@gmail.com>
2023-10-12 09:16:40 +0900

    카드 게임 코드 간소화

---
+++ src/assets/db/cardData.ts
@@ -0,0 +1,6 @@
+export const cardData = [
+  { id: 1, img: '/GGwangCard.png', isAnswer: false },
+  { id: 2, img: '/GGwangCard.png', isAnswer: false },
+  { id: 3, img: '/GGwangCard.png', isAnswer: false },
+  { id: 4, img: '/SuccessJinro.png', isAnswer: true },
+];
