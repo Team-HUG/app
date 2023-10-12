28867589ceb015856fdc6ac40526a7ec801441f1
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-10-12 15:00:11 +0900

    틀린그림 찾기

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,22 @@ import OrderPage from './pages/OrderPage';
 import Prepare from './pages/Prepare';
 import AdminOrderList from './pages/AdminOrderList';
 import AdminGameList from './pages/AdminGameList';
-import styled from 'styled-components';
-import Main from './pages/Main';
-import { ToastContainer, toast } from 'react-toastify';
-import Modal from './components/common/modal';
+import FindWrongPicture from './pages/FindWrongPicture';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <StyledToastify autoClose={1000} position={toast.POSITION.TOP_RIGHT} />
-      <Modal />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<div />} />
         <Route path="/order" element={<OrderPage />} />
         <Route path="/card-game" element={<CardGamePage />} />
         <Route path="/admin/order-list" element={<AdminOrderList />} />
         <Route path="/admin/game-list" element={<AdminGameList />} />
+        <Route path="/game/find-wrong" element={<FindWrongPicture />} />
         <Route path="/prepare" element={<Prepare />} />
       </Routes>
     </BrowserRouter>
   );
 };
-
-const StyledToastify = styled(ToastContainer)``;
 
 export default App;
